114eefe1a2aa8f400315fc6a277ec11786fb6ce0
CIN-TEST <s1120372@dmhs.kh.edu.tw>
2025-09-18 07:09:02 +0000

    轉換溫度

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-int main(){
-    std::cout << "HELLO world";
-    return 0;
+using namespace std;
+
+// 華氏攝氏轉換
+int main() {
+    double fahrenheit;
+    double celsius;
     
+    cout << "請輸入華氏溫度:\n"; // Prompt user for Fahrenheit input
+    cin >> fahrenheit; // Get Fahrenheit input from user
+    
+    // Convert Fahrenheit to Celsius
+    celsius = (fahrenheit - 32) / 9 * 5;
+    
+    // Output the result
+    cout << "攝氏溫度是:" << celsius;
+    return 0;
 }
