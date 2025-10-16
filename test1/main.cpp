001e9e4a03df793c1b8e42ecd8153113d5ed9408
CIN-TEST <s1120372@dmhs.kh.edu.tw>
2025-10-16 14:34:39 +0800

    HW5

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,12 +1,28 @@
-/******************************************************************************
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0) cout << i << " ";
+    // 第一段：印出星星三角形
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
     }
+
+    cout << endl;  // 分隔線
+
+    // 第二段：九九乘法表
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+
     return 0;
 }
